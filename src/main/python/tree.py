fe8a21b1be4f26850572784d86f2c2d3bc3f52cf
Williame33445 <williame15254@gmail.com>
2020-08-25 11:35:12 +0100

    added comments

--- src/main/python/tree.py
+++ src/main/python/tree.py
@@ -14,7 +14,7 @@ class Particle:
 #allows you to create a particle without first having to create a position
 def particle(mass,x,y):
     return Particle(mass,Position(x,y))
-    
+
 #function that finds out what segment cors is in
 #0 - top left, 1 - top right, 2 bottom left, 3 - bottom right
 def quadrantNumber(cors,midpoint):
@@ -40,11 +40,15 @@ class Node:
         #if there are no particles in this node put the particle in here
         if self.particleCount == 0:
             self.particle = newParticle
-            """if there is another particle in this node then find the segmant
-            that is goes in and add to node"""
         else:
+            """if there are other particles in this node then find the segmant
+            that is goes in and add to node"""
+            #add particle to child node
             self.addToCorrectChild(newParticle)
+            """this recusively runs untill untill we get to a leaf node(bottom node) and extends it
+            until one bellow its own node"""
             if self.particle != None:
+                #does the same proccess as new particle until both particles have there own node
                 self.addToCorrectChild(self.particle)
                 #wipe the particle as we now have multiple particles in the same node
                 self.particle = None
@@ -52,26 +56,30 @@ class Node:
         self.particleCount += 1
 
     def addToCorrectChild(self,particle):
-        #find quadrent that the particle should go in 
+        #find quadrent that the particle should go in
         childIndex = quadrantNumber(particle.pos,self.midPoint)
-        # Get the child
+        """if the child isn't occupied it returns none and calculates the childs midpoint and halfWidth
+        otherwise it returns the values of the particles that occupy it"""
         child = self.getOrCreateChildAt(childIndex)
-        
+        """recursion then happens again and the entire proccess continues to happen until the particles
+        each have there own node"""
         child.addParticle(particle)
 
     def getOrCreateChildAt(self, childIndex):
-        #if the child index is not occupied 
+        #we define (0,0) at the center of the screen
+        #if the child index is not occupied
         if self.children[childIndex] == None:
             #calculate the width of the child
             childHalfWidth = self.halfWidth / 2.0
             """calculate the new child's midpoint by adding or subtracting the
             appropriate offsets"""
+            #are defined due to the way segments were defined
             deltaX = [-1, +1, -1, +1]
             deltaY = [-1, -1, +1, +1]
-            #if segement is 0 then the x coordinate will
+            #calculate an unscaled offset and then scale it
             offset = Position(deltaX[childIndex],deltaY[childIndex]).scaled(childHalfWidth)
-                
+            #translate position
             childMidpoint = self.midPoint.translated(offset)
+            #define the child
             self.children[childIndex] = Node(childMidpoint ,childHalfWidth)
-            
         return self.children[childIndex]
--- src/test/python/testing.py
+++ src/test/python/testing.py
@@ -1,9 +1,8 @@
-import sys 
+import sys
 import os
-sys.path.append(os.path.abspath("../../main/python"))
+sys.path.append(os.path.abspath("src/main/python"))
 
 from tree import *
-import random
 
 lst = [particle(1,-3,-3),particle(1,-2,-3)]
 
@@ -11,9 +10,8 @@ def buildTree(lst,rootNode):
     for x in range(len(lst)):
         rootNode.addParticle(lst[x])
     print(rootNode.children)
-              
+
 rootNode = Node(Position(0,0),5)
 buildTree(lst,rootNode)
 
-#change names at some point
-
+#change names at some
