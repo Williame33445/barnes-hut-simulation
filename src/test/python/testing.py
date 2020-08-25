13e3b69a0780158edf2f9ba2b3a517f7b5ed94d8
Williame33445 <williame15254@gmail.com>
2020-08-25 11:57:47 +0100

    print tree

--- src/test/python/testing.py
+++ src/test/python/testing.py
@@ -4,12 +4,20 @@ sys.path.append(os.path.abspath("src/main/python"))
 
 from tree import *
 
+def printTree(t,depth=0):
+    print(depth*" " + "center=(" + str(t.midPoint.x)+","+str(t.midPoint.y)+")" + ",halfWidth=" +
+    str(t.halfWidth) + ",numberOfChildren=" + str(t.particleCount-1))
+    for x in range(t.particleCount):
+        if t.children[x] != None:
+            printTree(t.children[x],depth+1)
+
+
 lst = [particle(1,-3,-3),particle(1,-2,-3)]
 
 def buildTree(lst,rootNode):
     for x in range(len(lst)):
         rootNode.addParticle(lst[x])
-    print(rootNode.children)
+    printTree(rootNode)
 
 rootNode = Node(Position(0,0),5)
 buildTree(lst,rootNode)
