3cbeaa1192ada01a5e197e6cd325b4a4d52420cd
Williame33445 <williame15254@gmail.com>
2021-02-15 12:33:53 +0000

    added run

--- barneshut/GUI/simulationCreator.py
+++ barneshut/GUI/simulationCreator.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 sys.path.append(os.path.abspath("."))
 
 from barneshut.getSimulation import *
+from barneshut.simulationCommand import *
 
 simulationParamsOptions = ["Half Width","Tick Period","Total Duration","Theta","Max Depth"]
 viewParamsOptions = ["Width","Zoom","Mass Factor"]
@@ -14,24 +15,48 @@ viewParamsOptions = ["Width","Zoom","Mass Factor"]
 master = tk.Tk()
 master.geometry("1000x300")
 
+#need to create an option for this
+folderLocation = "C:\\workspace\\git-repos\\barnes-hut-simulation\\barneshut\\data\\data1"
+FPS = 24
+
+def getSimulationParamsForScreen(data):
+    return SimulationParams(float(data[0].get()),float(data[1].get()),float(data[2].get()),float(data[3].get()),float(data[4].get()))
+
+def getViewParamsForScreen(data):
+    return ViewParams(float(data[0].get()),float(data[1].get()),float(data[2].get()))
+
+
 class Page:
     def __init__(self,forms,fileName):
         #both of them are lists 
         self.forms = forms
         self.fileName = fileName
         self.factor = len(fileName)
-
+        #0 is run and show, 1 in simulate to file
+        self.runType = tk.IntVar()
 
     def runForms(self):
         for x in range(len(self.fileName)):
             self.forms[x].createForm()
         tk.Button(master,text='Save', command=self.saveAll).place(x=10,y=50*self.factor)
         tk.Button(master,text="Load",command=self.load).place(x=55,y=50*self.factor)
+        tk.Button(master,text="Run",command=self.run).place(x=100,y=50*self.factor)
+        tk.Checkbutton(master,text="Run to file",variable=self.runType).place(x=145,y=50*self.factor)
 
     def load(self):
-        folderSelected = filedialog.askdirectory()
-        barnesHutCLI(folderSelected,runType,fileName)
+        pass
 
+    def run(self):
+        simulationParams = getSimulationParamsForScreen(self.forms[0].data)
+        viewParams = getViewParamsForScreen(self.forms[1].data)
+        #need to change at some point
+        particles = getParticles(folderLocation + "\\particles.csv")
+        if self.runType.get() == 0:
+            command = SimulateAndShow(particles,simulationParams,viewParams,'Circle')
+            command.execute()
+        elif self.runType.get() == 1:
+            command = SimulateToFile(particles,simulationParams,viewParams,folderLocation+"\\data.mp4",FPS)
+            command.execute()
 
     def saveAll(self):
         for x in range(len(self.fileName)):
@@ -74,7 +99,7 @@ class Form:
         for x in self.options:
             line = line + x + ","
         return line
-
+    
 
 
 
