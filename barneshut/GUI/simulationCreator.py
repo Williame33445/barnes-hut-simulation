26f6f6d122b7022e674f2ec287f1769f24c9ca7b
Williame33445 <williame15254@gmail.com>
2021-02-16 11:19:55 +0000

    changed names

--- barneshut/GUI/simulationCreator.py
+++ barneshut/GUI/simulationCreator.py
@@ -39,18 +39,19 @@ def emptyTkList(length):
     return lst
 
 class Page:
-    def __init__(self,forms,fileName):
-        #both of them are lists 
-        self.forms = forms
-        self.fileName = fileName
+    def __init__(self,viewParametersForm,simulationParametersForm):
+        #sim is 1 view is 2
+        self.viewParametersForm = viewParametersForm
+        self.simulationParametersForm = simulationParametersForm
+        self.fileName = "\\data.mp4"
         self.factor = 2
         #0 is run and show, 1 in simulate to file
         self.runType = tk.IntVar()
         self.folderLocation = "C:\\workspace\\git-repos\\barnes-hut-simulation\\barneshut\\data\\data1"
 
     def runForms(self):
-        for x in range(len(self.fileName)):
-            self.forms[x].createForm()
+        self.viewParametersForm.createForm()
+        self.simulationParametersForm.createForm()
         tk.Button(master,text='Save', command=self.saveAll).place(x=10,y=50*self.factor)
         tk.Button(master,text="Load",command=self.load).place(x=55,y=50*self.factor)
         tk.Button(master,text="Run",command=self.run).place(x=100,y=50*self.factor)
@@ -58,34 +59,33 @@ class Page:
 
     def load(self):
         simulationParamsData = getData(self.folderLocation + "\\simulationParams.csv",1)
-        self.forms[0].setData(simulationParamsData)
+        self.simulationParametersForm.setData(simulationParamsData)
         viewParamsData = getData(self.folderLocation + "\\viewParams.csv",1)
-        self.forms[1].setData(viewParamsData)
+        self.viewParametersForm.setData(viewParamsData)
 
     def run(self):
-        simulationParams = getSimulationParamsForScreen(self.forms[0])
-        viewParams = getViewParamsForScreen(self.forms[1])
-        print(simulationParams.halfWidth)
+        simulationParams = getSimulationParamsForScreen(self.simulationParametersForm)
+        viewParams = getViewParamsForScreen(self.viewParametersForm)
         #need to change at some point
         particles = getParticles(self.folderLocation + "\\particles.csv")
         if self.runType.get() == 0:
             command = SimulateAndShow(particles,simulationParams,viewParams,'Circle')
             command.execute()
         elif self.runType.get() == 1:
-            command = SimulateToFile(particles,simulationParams,viewParams,self.folderLocation+"\\data.mp4",FPS)
+            command = SimulateToFile(particles,simulationParams,viewParams,self.folderLocation+self.fileName,FPS)
             command.execute()
 
     def saveAll(self):
-        for x in range(len(self.fileName)):
-            self.saveToFile(x)
+        self.saveToFile(self.folderLocation + "\\simulationParams.csv",self.simulationParametersForm)
+        self.saveToFile(self.folderLocation + "\\viewParams.csv",self.viewParametersForm)
 
 
-    def saveToFile(self,t):
-        file = open(self.fileName[t],"w")
-        file.write(self.forms[t].printHeaders() + "\n")
-        for x in range(len(self.forms[t].options)):
+    def saveToFile(file,form):
+        file = open(file,"w")
+        file.write(form.printHeaders() + "\n")
+        for x in range(len(form.options)):
             try:
-                intData = int(self.forms[t].data[x].get().strip())
+                intData = int(form.data[x].get().strip())
                 file.write(str(intData))
                 file.write(",")
             except:
@@ -125,8 +125,7 @@ class Form:
 
 
 temp = [Form(simulationParamsOptions,0,"Simulation Parameters"),Form(viewParamsOptions,2,"View Parameters")]
-fileNames = ["simulationParams.csv","viewParams.csv"]
-var = Page(temp,fileNames)
+var = Page(temp[1],temp[0])
 var.runForms()
 
 
